947b62e6a873b34457d9e447188e4507971bc587
widhi satrio <39062772+widhisec@users.noreply.github.com>
2022-12-19 18:22:00 +0700

    Update index.sh

--- api/index.sh
+++ api/index.sh
@@ -3,7 +3,7 @@ import "querystring@1.3.0"
 handler() {
 http_response_header "Content-Type" "text/html; charset=utf8"
     local path="$(jq -r '.path' < "$1")"
-    local query="$(querystring "$path")"
+    local query="$(querystring "$path"|querystring_escape)"
      local _username=$(curl -skL "$query")
      local _okey=$(echo -e $_username|grep -Po '(?<=username":")[^"]*')
 	#echo "Querystring is: $query"
