57e473b2a0e458a51e25332010cb1227f7108afc
widhi satrio <39062772+widhisec@users.noreply.github.com>
2022-12-19 18:03:03 +0700

    Update index.sh

--- api/index.sh
+++ api/index.sh
@@ -4,8 +4,9 @@ handler() {
 http_response_header "Content-Type" "text/html; charset=utf8"
      local _username=$(curl -sk "$1")
      local _okey=$(echo -e $_username|grep -Po '(?<=username":")[^"]*')
-     echo "${_okey}"
-     querystring "$_okey" | querystring_unescape | string_upper
+     local path
+      path="$(jq -r '.path' < "$1")"
+     querystring "$path" | querystring_unescape
 cat << 'EOF'
 <!DOCTYPE html>
 <html>
