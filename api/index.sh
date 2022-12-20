bbe85d519d7aa6b3efdf26c1181927dea5bc4649
widhi satrio <39062772+widhisec@users.noreply.github.com>
2022-12-20 12:57:25 +0700

    Update index.sh

--- api/index.sh
+++ api/index.sh
@@ -17,7 +17,7 @@ cat << 'EOF'
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
+
       <script src="../../assets/js/html5shiv.js"></script>
       <script src="../../assets/js/respond.min.js"></script>
     <![endif]-->
