f861b8e774fa9014c05bfe963954a9c47aa70b2b
widhi satrio <39062772+widhisec@users.noreply.github.com>
2022-12-21 15:45:10 +0700

    Update index.sh

--- api/index.sh
+++ api/index.sh
@@ -8,27 +8,5 @@ http_response_header "Content-Type" "text/html; charset=utf8"
      local _okey=$(echo -e $_username|grep -Po '(?<=username":")[^"]*')
 	#echo "Querystring is: $query"
 	echo "$_okey"
-cat << 'EOF'
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="//code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-  </body>
-</html>
-EOF
 	
 }
