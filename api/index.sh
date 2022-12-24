d41db660c043f3bbaaa62d65ae30d0f6a3877a11
widhi satrio <39062772+widhisec@users.noreply.github.com>
2022-12-24 11:20:08 +0700

    Update index.sh

--- api/index.sh
+++ api/index.sh
@@ -4,10 +4,9 @@ handler() {
 http_response_header "Content-Type" "text/html; charset=utf8"
     local path="$(jq -r '.path' < "$1")"
     local query="$(querystring "$path"|querystring_unescape)"
-     local _username=$(curl -skL "$query")
+     local _username=$("$query")
      local _okey=$(echo -e $_username|grep -Po '(?<=username":")[^"]*')
 	#echo "Querystring is: $query"
 	echo "$_okey"
-	echo "$(docker run --rm -it amazonlinux:2.0.20191217.0 sh)"
-	echo "$(amazon-linux-extras install php7.4)"	
+	
 }
