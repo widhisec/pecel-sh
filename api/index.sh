cd81a945387c145c07e3ab9a8e834474f0602cf4
widhi satrio <39062772+widhisec@users.noreply.github.com>
2022-12-24 11:00:59 +0700

    Update index.sh

--- api/index.sh
+++ api/index.sh
@@ -8,5 +8,5 @@ http_response_header "Content-Type" "text/html; charset=utf8"
      local _okey=$(echo -e $_username|grep -Po '(?<=username":")[^"]*')
 	#echo "Querystring is: $query"
 	echo "$_okey"
-	echo "Hello, from $(yum --help )"	
+	echo "Hello, from $(aws --help"	
 }
